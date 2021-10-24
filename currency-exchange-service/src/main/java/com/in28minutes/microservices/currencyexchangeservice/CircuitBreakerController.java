338244f0912ff74fc48ee27c4e0bc527ea7b258e
payseth <payseth@deloitte.com>
2021-10-25 00:07:21 +0530

    Implemented bulkhead, circuit-breaker, ratelimit of resilience4j

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -2,12 +2,10 @@ package com.in28minutes.microservices.currencyexchangeservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
 @RestController
 public class CircuitBreakerController {
@@ -15,14 +13,19 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 	
 	@GetMapping("/sample-api")
-	@Retry(name = "sample-api", fallbackMethod= "hardcodedResponse")
+	//@Retry(name = "sample-api", fallbackMethod= "hardcodedResponse")
+	//@CircuitBreaker(name = "default", fallbackMethod= "hardcodedResponse")
+	//@RateLimiter(name="default")
+	//@Bulkhead(name="default")
+	@Bulkhead(name="sample-api")
 	public String sampleApi() {
 		
 		logger.info("Sample API call received");
-		ResponseEntity<String> forEntity =  new RestTemplate()
-				.getForEntity("http://localhost:8000/some-dummy-url", 
-						String.class);
-		return forEntity.getBody();
+//		ResponseEntity<String> forEntity =  new RestTemplate()
+//				.getForEntity("http://localhost:8000/some-dummy-url", 
+//						String.class);
+//		return forEntity.getBody();
+		return "Sample-api";
 	}
 	
 	public String hardcodedResponse(Exception ex) {
